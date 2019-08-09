291cde5442d63f68a817e41514dc97c59606a874
User <17693119+vindard@users.noreply.github.com>
2019-08-09 16:26:30 -0400

    move requirements into `setup.py`

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ setup(
     name='Stream Tools',
     version='0.1dev',
     packages=['streamtools',],
+    install_requires=[
+        'aiokafka',
+        'aio-pika',
+        'aiohttp',
+        'requests',
+      ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
 )
