18bfdda93e67871cc76751ef7832ca40e5da1003
User <17693119+vindard@users.noreply.github.com>
2020-01-31 16:28:09 -0400

    remove unnecessary `httpsender` object

--- streamtools/__init__.py
+++ streamtools/__init__.py
@@ -1,5 +1,2 @@
 from .queues import HandlerFactory, ConsumerRunner, CONSUMER_LOOPS, PRODUCERS
 from .libs import prepare_aiohttp_app
-
-# Create agnostic post handler to be used to send POST requests
-httpsender = PRODUCERS["HTTPHandler"]()
