ecb7fc17e03f9afe28d6f083f2b44d1161d8ed47
Louis Kinley <louis@caripay.net>
2021-11-27 13:14:44 -0400

    Updated package versions to nov 1, 2021 compat

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='streamtools',
-    version='1.1.1',
+    version='0.1dev',
     packages=['streamtools',],
     install_requires=[
-        'aiohttp>=3.7.4',
-        'aiokafka>=0.5.1',
-        'aio-pika>=5.6.0',
-        'boto3==1.20.8',
+        'aiohttp==3.7.4',
+        'aiokafka==0.5.1',
+        'aio-pika==5.6.0',
+        'boto3==1.19.8',
         'aiobotocore==2.0.1',
       ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
