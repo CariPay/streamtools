4f16f6ebe1e9bbfe418235546f9f946bf5301fd2
DarionHernandez <darionhernandez868@gmail.com>
2025-06-06 07:10:52 -0400

    Added kafka python

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         'aiohttp>=3.8.0,<4.0.0',
         'aiokafka>=0.8.0,<1.0.0',
+        'kafka-python>=2.0.0',
         'aio-pika>=8.0.0,<10.0.0',
         'boto3>=1.26.0,<2.0.0',
         'aiobotocore>=2.5.0,<3.0.0',
