6104dc7b4e1b293f905bb52c55676800ac72134c
Darrion Mills <dmills1994@gmail.com>
2025-10-10 11:39:44 -0400

    update boto3 version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='streamtools',
-    version='0.1dev',
+    version='1.1.2',
     packages=['streamtools',],
     install_requires=[
         'aiohttp==3.13.0',
         'aiokafka==0.5.1',
         'aio-pika==5.6.0',
-        'boto3==1.19.8',
+        'boto3==1.39.0',
         'aiobotocore==2.0.1',
       ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
