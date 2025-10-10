c675750f383629cfb602597976d9f46ac4b2b77c
Darrion Mills <dmills1994@gmail.com>
2025-10-10 15:40:54 -0400

    update boto3 to satisfy aiobotocore dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'aiohttp==3.13.0',
         'aiokafka==0.5.1',
         'aio-pika==5.6.0',
-        'boto3==1.39.0',
+        'boto3==1.40.49',
         'aiobotocore==2.25.0',
       ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
